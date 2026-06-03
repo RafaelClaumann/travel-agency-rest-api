6803edff8ef0cc8e8c81a3942e7a4e65e847e560
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2026-06-03 19:28:03 -0300

    Create TravelPackage repository

---
+++ src/main/java/org/claumann/travelagency/repository/TravelPackageRepository.java
@@ -0,0 +1,10 @@
+package org.claumann.travelagency.repository;
+
+import org.claumann.travelagency.entity.TravelPackage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TravelPackageRepository extends JpaRepository<TravelPackage, Long> {
+
+}

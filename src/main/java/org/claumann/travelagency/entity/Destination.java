708defc31ff727987bb53e1e58ecd04600845b71
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2026-06-03 19:24:21 -0300

    Create Destination and TravelPackage JPA entities

---
+++ src/main/java/org/claumann/travelagency/entity/Destination.java
@@ -0,0 +1,32 @@
+package org.claumann.travelagency.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "destination")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Destination {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String location;
+    private String description;
+
+    private Double averageRating;
+    private Integer totalRatings;
+
+}
---
+++ src/main/java/org/claumann/travelagency/entity/TravelPackage.java
@@ -0,0 +1,35 @@
+package org.claumann.travelagency.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "travel_package")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TravelPackage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private Double price;
+
+    @ManyToOne
+    @JoinColumn(name = "destination_id")
+    private Destination destination;
+
+}

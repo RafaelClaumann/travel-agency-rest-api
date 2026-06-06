67b26b9b24355ced5728477314a7dcda305923c7
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2026-06-05 21:29:48 -0300

    Implement global exception handling with RestExceptionHandler and ErrorResponse DTO

---
+++ src/main/java/org/claumann/travelagency/exception/dto/ErrorResponse.java
@@ -0,0 +1,29 @@
+package org.claumann.travelagency.exception.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+
+    public ErrorResponse(HttpStatus status, String message, String path) {
+        this.timestamp = LocalDateTime.now();
+        this.status = status.value();
+        this.error = status.getReasonPhrase();
+        this.message = message;
+        this.path = path;
+    }
+}

3b00de50bc45bd574d0240c2af68782b5881821f
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2026-06-10 21:01:34 -0300

    Rename UserEntity table to 'users' and seed initial user accounts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -16,3 +16,13 @@ VALUES ('Arraial Completo', 'Passeio de barco, mergulho e praias em 5 dias.', 25
        ('Floripa em Família', 'Roteiro pelas principais praias e pontos turísticos em 6 dias.', 2800.00, 3),
        ('Lençóis Inesquecível', 'Expedição pelas dunas e lagoas em 5 dias.', 3800.00, 4),
        ('Bonito Selvagem', 'Flutuação, rapel e trilhas em 4 dias no coração do Pantanal.', 2900.00, 5);
+
+-- senha: admin123
+-- System.out.println("sua senha: " + new BCryptPasswordEncoder().encode("admin123"));
+INSERT INTO users (username, password, role) VALUES
+    ('admin', '$2a$10$5SOmfbYz6cHkoA0lrfHwmuo/csL9fbmsyUtCb/F/rU58NbEGDLgKC', 'ADMIN');
+
+-- senha: user123
+-- System.out.println("sua senha: " + new BCryptPasswordEncoder().encode("user123"));
+INSERT INTO users (username, password, role) VALUES
+    ('user', '$2a$10$FDUo5FtveLowBecIfsnDZuhoh9o/XScj1Bb/xlkc5GtTlZAUa5CC6', 'USER');

92bcda418f5dd6e730aea0a6b9d23e0e47112d80
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2026-06-16 12:15:44 -0300

    Implement JwtAuthenticationFilter authorization

--- src/main/java/org/claumann/travelagency/security/JwtAuthenticationFilter.java
+++ src/main/java/org/claumann/travelagency/security/JwtAuthenticationFilter.java
@@ -6,6 +6,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -15,6 +18,15 @@ import java.io.IOException;
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+
+    private final UserDetailsServiceImpl userDetailsService;
+    private final TokenService tokenService;
+
+    public JwtAuthenticationFilter(UserDetailsServiceImpl userDetailsService, TokenService tokenService) {
+        this.userDetailsService = userDetailsService;
+        this.tokenService = tokenService;
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         final String jwtToken = request.getHeader(HttpHeaders.AUTHORIZATION);
@@ -22,6 +34,14 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         if (jwtToken == null || !jwtToken.startsWith("Bearer")) {
             filterChain.doFilter(request, response);
             return;
+        }
+
+        String token = jwtToken.substring(7); // remove "Bearer "
+        String username = tokenService.validateToken(token);
+        if (username != null) {
+            UserDetails user = userDetailsService.loadUserByUsername(username);
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
         }
 
         log.info("Found token in header [{}]", jwtToken);

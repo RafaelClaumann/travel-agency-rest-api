fdbc9d734b3ef7d629850cb19cc4031036bd3ab9
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2026-06-17 17:59:27 -0300

    Refactor AuthController to delegate login logic to LoginUseCaseImpl

--- src/main/java/org/claumann/travelagency/security/AuthController.java
+++ src/main/java/org/claumann/travelagency/security/AuthController.java
@@ -1,37 +1,25 @@
 package org.claumann.travelagency.security;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
 @RequestMapping("/auth/login")
 public class AuthController {
 
-    private final AuthenticationManager authenticationManager;
-    private final TokenService tokenService;
+    private final LoginUseCaseImpl loginUseCase;
 
-    public AuthController(AuthenticationManager authenticationManager, TokenService tokenService) {
-        this.authenticationManager = authenticationManager;
-        this.tokenService = tokenService;
+    public AuthController(LoginUseCaseImpl loginUseCase) {
+        this.loginUseCase = loginUseCase;
     }
 
     @PostMapping
     public ResponseEntity<AuthResponse> login(@RequestBody final AuthRequest authRequest) {
-        var authentication = new UsernamePasswordAuthenticationToken(authRequest.username(), authRequest.password());
-        var authenticate = authenticationManager.authenticate(authentication);
-
-        var principal = (UserDetails) authenticate.getPrincipal();
-        log.info("Generated token [{}]", tokenService.generateToken(principal));
-
-        return ResponseEntity.ok(new AuthResponse(tokenService.generateToken(principal)));
+        var authResult = loginUseCase.execute(authRequest);
+        return ResponseEntity.ok(authResult);
     }
 
 }
